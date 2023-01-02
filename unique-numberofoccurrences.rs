c1dda6c3b19e998dc2c0d6aa333b82a49c28dd68
Ruben <83189955+0x17io@users.noreply.github.com>
2023-01-02 14:23:35 -0600

    Create unique-numberofoccurrences.rs

---
+++ unique-numberofoccurrences.rs
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/unique-number-of-occurrences/
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn unique_occurrences(arr: Vec<i32>) -> bool {
+        let mut heap: HashMap<i32, i32> = HashMap::new();
+        
+        for num in arr {
+            if !heap.contains_key(&num) {
+                heap.insert(num, 1);
+            }
+            else {
+                heap.insert(num, heap[&num] + 1);
+            }
+        }
+
+
+        let mut stack: Vec<i32> = Vec::new();
+
+        for item in heap.values() {
+            if !stack.contains(item) {
+                stack.push(*item);  
+            }
+            else {
+                return false;
+            } 
+        }
+
+
+        true
+    }
+}
